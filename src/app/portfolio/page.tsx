1487a28ed89667d5583f4e0e48b00e75de607b68
KeamKRUB <pawatchijaroen2004@gmail.com>
2024-04-15 05:58:41 +0700

    Finish2

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,12 +1,9 @@
-"use client"
+import { motion, useScroll, useTransform } from 'framer-motion';
+import { useRef } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 
-import {motion, useScroll, useTransform} from 'framer-motion'
-import {useRef} from 'react'
-import Image from 'next/image'
-import Link from 'next/link'
-
-
-const portfolio = () => {
+const Portfolio = () => {
     const items = [
         {
             id: 1,
@@ -54,48 +51,45 @@ const portfolio = () => {
             github: "https://github.com/KeamKRUB/Portfolio_Website"
         }
     ]
-    const ref = useRef(null)
-    const {scrollYProgress} = useScroll({target:ref})
-    const x = useTransform(scrollYProgress,[0,1],["0%","-67%"])
+    const ref = useRef(null);
+    const {scrollYProgress} = useScroll({target:ref});
+    const x = useTransform(scrollYProgress,[0,1],["0%","-67%"]);
     
     return(
-        <motion.div className=" h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:0.8}}>
-        <div className="h-[600vh] relative" ref={ref} >
-            <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-center text-8xl">
-            My Portfolio
-            </div>
-            <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
-                <motion.div style={{x}} className="flex">
-                <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-white to-blue-300">
-
+        <motion.div className="h-full" initial={{y:"-200vh"}} animate={{y:"0%"}} transition={{duration:0.8}}>
+            <div className="h-[600vh] relative" ref={ref}>
+                <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-center text-8xl">
+                    My Portfolio
                 </div>
-                {items.map(item=>(
-                    <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id}>
-                        <div className="flex flex-col gap-8 text-white flex items-center jestify-center text-center">
-                          <h1 className="font-bold text-4xl">{item.title}</h1>  
-                          <div className="">
-                            <Image src="/cat.png" alt="" width={400} height={400}/>
-                          </div>
-                          <p className="bg-white text-black rounded-xl p-4 w-2/3">
-                            {item.desc}
-                          </p>
-                          <div className="flex gap-12">
-                          <Link href={item.link} className="p-3 font-semibold bg-white text-black rounded-lg">
-                            Preview
-                          </Link>
-                          <Link href={item.github} className="p-3 font-semibold bg-black text-white rounded-lg">
-                            Github
-                          </Link>
-                          </div>
-                        </div>
-                    </div>
-                ))}
-                </motion.div>
+                <div className="sticky top-0 flex h-screen gap-4 items-center overflow-hidden">
+                    <motion.div style={{x}} className="flex">
+                        <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-white to-blue-300"></div>
+                        {items.map(item=>(
+                            <div className={`h-screen w-screen flex items-center justify-center bg-gradient-to-r ${item.color}`} key={item.id}>
+                                <div className="flex flex-col gap-8 text-white flex items-center jestify-center text-center">
+                                    <h1 className="font-bold text-4xl">{item.title}</h1>  
+                                    <div className="">
+                                        <Image src="/cat.png" alt="" width={400} height={400}/>
+                                    </div>
+                                    <p className="bg-white text-black rounded-xl p-4 w-2/3">
+                                        {item.desc}
+                                    </p>
+                                    <div className="flex gap-12">
+                                        <Link href={item.link} className="p-3 font-semibold bg-white text-black rounded-lg">
+                                            Preview
+                                        </Link>
+                                        <Link href={item.github} className="p-3 font-semibold bg-black text-white rounded-lg">
+                                            Github
+                                        </Link>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </motion.div>
+                </div>
             </div>
-        </div>
         </motion.div>
-    )
-
+    );
 }
 
-export default portfolio
+export default Portfolio;
